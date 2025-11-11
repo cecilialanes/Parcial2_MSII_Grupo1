5af776bff064f22270998c55928540f5fb27c1ae
ProgramacionCudi <fox1234567890@hotmail.com>
2025-11-11 19:45:22 -0300

    Agrego funcion y test dividir_olguin segun formato PDF

---
+++ funciones/dividir_olguin.py
@@ -0,0 +1,9 @@
+# Funcion de division - integrante olguin
+# Autor: olguin
+# Mail: evanataliaolguin4@gmail.com
+
+def dividir_olguin(a, b):
+    """Devuelve la division de dos numeros. Si b es 0, devuelve None.""" 
+    if b == 0:
+        return None
+    return a / b
---
+++ test/test_dividir_olguin.py
@@ -0,0 +1,4 @@
+from funciones.dividir_olguin import dividir_olguin
+
+def test_dividir_olguin():
+    assert dividir_olguin(5, 0) is None
